d49c72b3475c5aca876800f258c1f49a17fcac60
Tomi Sarparanta <tomi.sarparanta@sofiadigital.com>
2021-11-05 15:26:48 +0200

    backend url as environment variable

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.VUE_APP_BACKEND_URL,
   headers: {
     "Content-type": "application/json"
   }
