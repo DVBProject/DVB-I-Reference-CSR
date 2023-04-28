36b8bb4cacbbd7f2bf7cd5fe014bf57dca4aa408
Tomi Sarparanta <tomi.sarparanta@sofiadigital.com>
2023-04-28 09:57:08 +0300

    added logo images, footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <nav class="navbar navbar-expand navbar-dark bg-dark px-2">
+      <img class="logo" src="@/assets/logo_dvb-i.png" />
       <router-link to="/" class="navbar-brand">DVB-I CSR</router-link>
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav mr-auto">
@@ -45,12 +46,22 @@
           -->
         </ul>
       </div>
-      <span class="navbar-text">Version: {{ version }}, Built: {{ buildDate }} </span>
+      <span class="navbar-text"><img class="logo" src="@/assets/logo_sofiadigital.png"/></span>
     </nav>
 
     <div class="container mt-3">
       <router-view />
     </div>
+    <footer class="fixed-bottom footer">
+      <div class="row my-2">
+        <div class="text-center col-lg-6 offset-lg-3">
+          <p>
+            <a href="https://sofiadigital.com/">Sofia Digital Ltd.</a><br />
+            <small>Version:{{ version }}, Built: {{ buildDate }}</small>
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
@@ -59,7 +70,7 @@
     name: "App",
     data() {
       return {
-        version: "0.15",
+        version: "0.16",
       };
     },
     computed: {
@@ -109,5 +120,17 @@
   }
   .navbar-brand {
     text-decoration: none !important;
+  }
+  .logo {
+    height: 40px;
+  }
+  .footer {
+    height: 60px;
+    background-color: #011927;
+    color: #fff;
+  }
+
+  body {
+    margin-bottom: 60px !important;
   }
 </style>
